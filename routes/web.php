a76e33d83f3bdbb8c6cac3c501e30deddee8b141
ninocphilip237 <ninocphilip237@gmail.com>
2021-12-23 12:20:08 +0530

    data

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+//use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+use App\Http\Controllers\StudentController;
+
+Route::get('add-student', [StudentController::class, 'create']);
+Route::post('add-student', [StudentController::class, 'store']);
