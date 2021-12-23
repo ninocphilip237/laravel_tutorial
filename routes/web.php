ac15a549d27f77e1c813a3f1a2e60e15f84b7f70
ninocphilip237 <ninocphilip237@gmail.com>
2021-12-23 11:55:55 +0530

    defaukt

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('dashboard');
+    return view('welcome');
 });
-//Route::post('dashboard',['uses'=>'dashboard-controller@dashboardfn']);
